bc26a739688a12a86718165cfb4de640db4333bd
karanrajput2003 <97956908+karanrajput2003@users.noreply.github.com>
2024-12-02 19:24:26 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -47,6 +47,9 @@ const Contact = () => {
   return (
     <div id="contact" className="my-20 min-h-96 w-screen px-10">
       <div className="relative rounded-lg bg-black py-24 text-blue-50 sm:overflow-hidden">
+        <p className="font-general text-sm uppercase md:text-[10px] text-center">
+          Contact Us
+        </p>
          <div className="flex flex-col items-center text-center">
           <AnimatedTitle
             title="Get in <b>T</b>ouch"
